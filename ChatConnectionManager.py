06389f6275af851a54cffd8f605b030fd2d2490c
kukukodes <45627511+Kukupie123@users.noreply.github.com>
2021-10-23 23:20:00 +0530

    working backend but need to send json body later

--- ChatConnectionManager.py
+++ ChatConnectionManager.py
@@ -24,31 +24,44 @@ class ChatConnectionManager:
                 if user['userWebSocket'] == client:
                     await client.send_text("Already connected")
                     return
+
             userName = data2['userName']
             # adding user to list
+            oldRN = self.roomNumber
             dic = {'userWebSocket': client, 'roomNumber': self.roomNumber,
                    'userName': userName}
+            self.users.append(dic)
 
+            self.roomNumber = self.roomNumber + 1
             sendDic = {
-                'roomNumber': self.roomNumber,
+                'roomNumber': oldRN,
                 'userName': userName,
                 'status code': 200
             }
             jsonString = str(json.dumps(sendDic))
-            self.roomNumber = self.roomNumber + 1
-            self.users.append(dic)
             # sending it back to client
-            await client.send_text(jsonString), 200
+            await client.send_text(jsonString)
 
         elif action == 'message':
+            # invalid data start
             roomNumberTemp = -1
+
+            # get room number of the sender
             for user in self.users:
                 if user['userWebSocket'] == client:
                     roomNumberTemp = user['roomNumber']
                     break
+            # continue this loop if roomNumberTemp is valid
             if roomNumberTemp != -1:
                 message = data2['message']
+                # for users who have same room number as the sender we are going to send the text
                 for user in self.users:
-                    await user['userWebSocket'].send_text(message)
+                    if user['roomNumber'] == roomNumberTemp:
+                        await user['userWebSocket'].send_text(message)
             else:
-                client.send_text("room doesnt exist yet")
+                await client.send_text("room doesnt exist yet")
+        elif action == 'join':
+            roomNumber = data2['roomNumber']
+            for user in self.users:
+                if user['roomNumber'] == roomNumber:
+                    await user
